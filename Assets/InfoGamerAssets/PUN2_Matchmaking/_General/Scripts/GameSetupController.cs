6df56f2b026589b745c107335f50889329ee1ad9
Stavrin <steffanleprince@gmail.com>
2024-10-03 22:18:47 +0100

    Adding Photon PUN 2 network multiplayer.

---
+++ Assets/InfoGamerAssets/PUN2_Matchmaking/_General/Scripts/GameSetupController.cs
@@ -0,0 +1,44 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameSetupController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject SpawnPoint;
+    public int ID;
+
+    // This script will be added to any multiplayer scene
+    void Start()
+    {
+        AssignPlayerID(); // Assign correct player ID based on whether the player is Master Client or not
+        CreatePlayer(ID);  // Create a networked player object for each player that loads into the multiplayer scene
+    }
+
+    private void AssignPlayerID()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            // Assign ID = 1 to the Master Client (Player 1)
+            ID = 1;
+        }
+        else
+        {
+            // Assign ID = 2 to the non-master client (Player 2)
+            ID = 2;
+        }
+
+        Debug.Log("Player assigned ID: " + ID);
+    }
+
+    private void CreatePlayer(int ID)
+    {
+        Debug.Log("Creating Player with ID: " + ID);
+        // Load the appropriate player prefab based on the assigned ID
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer" + ID),
+                                  SpawnPoint.transform.position,
+                                  Quaternion.identity);
+    }
+}
