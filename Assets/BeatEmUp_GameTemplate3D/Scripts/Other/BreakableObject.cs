e37a4b198ecf17f175cc0e8bdd32915166a365fc
Stavrin <steffanleprince@gmail.com>
2024-10-19 21:32:33 +0100

    Breakable objects syncing sometimes.
    Need to change it so that chance of spawning 100 percent as otherwise it's different in each client.

--- Assets/BeatEmUp_GameTemplate3D/Scripts/Other/BreakableObject.cs
+++ Assets/BeatEmUp_GameTemplate3D/Scripts/Other/BreakableObject.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
 public class BreakableObject : MonoBehaviour, IDamagable<DamageObject> {
@@ -16,44 +17,108 @@ public class BreakableObject : MonoBehaviour, IDamagable<DamageObject> {
 	[Space(10)]
 	public bool destroyOnHit;
 
-	void Start(){
-		gameObject.layer = LayerMask.NameToLayer("DestroyableObject");
+    private PhotonView photonView;
+
+    void Start(){
+
+        photonView = GetComponent<PhotonView>();
+
+        gameObject.layer = LayerMask.NameToLayer("DestroyableObject");
 	}
 
 	//this object was Hit
-	public void Hit(DamageObject DO){
+	public void Hit(DamageObject DO)
+	{
 
-		//play hit sfx
-		if (hitSFX != "") {
-			GlobalAudioPlayer.PlaySFXAtPosition (hitSFX, transform.position);
+		if (PhotonNetwork.IsConnected && photonView != null && photonView.IsMine)
+		{
+			photonView.RPC("RPC_HandleHit", RpcTarget.All, DO.inflictor != null ? DO.inflictor.transform.position.x : 0);
 		}
+		else
+		{
 
-		//spawn destroyed gameobject version
-		if (destroyedGO != null) {
-			GameObject BrokenGO = GameObject.Instantiate (destroyedGO);
-			BrokenGO.transform.position = transform.position;
-
-			//chance direction based on the impact direction
-			if (orientToImpactDir && DO.inflictor != null) {
-				float dir = Mathf.Sign(DO.inflictor.transform.position.x - transform.position.x);
-				BrokenGO.transform.rotation = Quaternion.LookRotation(Vector3.forward * dir);
+			//play hit sfx
+			if (hitSFX != "")
+			{
+				GlobalAudioPlayer.PlaySFXAtPosition(hitSFX, transform.position);
 			}
-		}
 
-		//spawn an item
-		if (spawnItem != null) {
-			if (Random.Range (0, 100) < spawnChance) {
-				GameObject item = GameObject.Instantiate (spawnItem);
-				item.transform.position = transform.position;
+			//spawn destroyed gameobject version
+			if (destroyedGO != null)
+			{
+				GameObject BrokenGO = GameObject.Instantiate(destroyedGO);
+				BrokenGO.transform.position = transform.position;
 
-				//add up force to object
-				item.GetComponent<Rigidbody>().velocity = Vector3.up * 8f;
+				//chance direction based on the impact direction
+				if (orientToImpactDir && DO.inflictor != null)
+				{
+					float dir = Mathf.Sign(DO.inflictor.transform.position.x - transform.position.x);
+					BrokenGO.transform.rotation = Quaternion.LookRotation(Vector3.forward * dir);
+				}
 			}
-		}
 
-		//destroy 
-		if (destroyOnHit) {
-			Destroy(gameObject);
+			//spawn an item
+			if (spawnItem != null)
+			{
+				if (Random.Range(0, 100) < spawnChance)
+				{
+					GameObject item = GameObject.Instantiate(spawnItem);
+					item.transform.position = transform.position;
+
+					//add up force to object
+					item.GetComponent<Rigidbody>().velocity = Vector3.up * 8f;
+				}
+			}
+
+			//destroy 
+			if (destroyOnHit)
+			{
+				Destroy(gameObject);
+			}
 		}
 	}
+
+    [PunRPC]
+    void RPC_HandleHit(float inflictorXPos)
+    {
+        // Play hit SFX
+        if (hitSFX != "")
+        {
+            GlobalAudioPlayer.PlaySFXAtPosition(hitSFX, transform.position);
+        }
+
+        // Spawn destroyed gameobject version
+        if (destroyedGO != null)
+        {
+            GameObject BrokenGO = GameObject.Instantiate(destroyedGO);
+            BrokenGO.transform.position = transform.position;
+
+            // Orient to impact direction
+            if (orientToImpactDir)
+            {
+                float dir = Mathf.Sign(inflictorXPos - transform.position.x);
+                BrokenGO.transform.rotation = Quaternion.LookRotation(Vector3.forward * dir);
+            }
+        }
+
+        // Spawn an item
+        if (spawnItem != null)
+        {
+            if (Random.Range(0, 100) < spawnChance)
+            {
+                GameObject item = GameObject.Instantiate(spawnItem);
+                item.transform.position = transform.position;
+
+                // Add upward force to object
+                item.GetComponent<Rigidbody>().velocity = Vector3.up * 8f;
+            }
+        }
+
+        // Destroy the object
+        if (destroyOnHit)
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
